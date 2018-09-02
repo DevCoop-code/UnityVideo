5cb9e9ee335aadec0dcf69944f9e05b0b68c940e
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-09-02 16:10:39 +0900

    feat: playing and pause

---
+++ Assets/VideoDemo/Scripts/WorldSpaceVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class WorldSpaceVideo : MonoBehaviour {
+
+	public Material playButtonMaterial;
+	public Material pauseButtonMaterial;
+	public Renderer playButtonRenderer;
+
+	private VideoPlayer videoPlayer;
+
+	void Awake(){
+		videoPlayer = GetComponent<VideoPlayer> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayPause(){
+		if (videoPlayer.isPlaying) 
+		{
+			videoPlayer.Pause ();
+			playButtonRenderer.material = playButtonMaterial;
+		} else 
+		{
+			videoPlayer.Play ();
+			playButtonRenderer.material = pauseButtonMaterial;
+		}
+	}
+}
