6f8ed06fdb2602ca13a465a6feb079b3c2220657
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-09-02 16:57:48 +0900

    feat: Displaying Current Tiem and Clip

--- Assets/VideoDemo/Scripts/WorldSpaceVideo.cs
+++ Assets/VideoDemo/Scripts/WorldSpaceVideo.cs
@@ -11,6 +11,11 @@ public class WorldSpaceVideo : MonoBehaviour {
 	public Renderer playButtonRenderer;
 	public VideoClip[] videoClips;
 
+	public Text currentMinutes;
+	public Text currentSeconds;
+	public Text totalMinutes;
+	public Text totalSeconds;
+
 	private VideoPlayer videoPlayer;
 	private int videoClipIndex;
 
@@ -24,7 +29,9 @@ public class WorldSpaceVideo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (videoPlayer.isPlaying) {
+			SetCurrentTimeUI ();
+		}
 	}
 
 	public void PlayPause(){
@@ -47,6 +54,23 @@ public class WorldSpaceVideo : MonoBehaviour {
 		}
 
 		videoPlayer.clip = videoClips [videoClipIndex];
+		SetTotalTimeUI ();
 		videoPlayer.Play ();
+	}
+
+	void SetCurrentTimeUI(){
+		string minutes = Mathf.Floor ((int)videoPlayer.time / 60).ToString ("00");
+		string seconds = ((int)videoPlayer.time % 60).ToString ("00");
+
+		currentMinutes.text = minutes;
+		currentSeconds.text = seconds;
+	}
+
+	void SetTotalTimeUI(){
+		string minutes = Mathf.Floor ((int)videoPlayer.clip.length / 60).ToString ("00");
+		string seconds = ((int)videoPlayer.clip.length % 60).ToString ("00");
+
+		totalMinutes.text = minutes;
+		totalSeconds.text = seconds;
 	}
 }
