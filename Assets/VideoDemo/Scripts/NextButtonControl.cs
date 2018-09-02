8203d5eec9752720c0497a29a49695e8fd4fa082
HanGyo Jeong <hangyojeong@HanGyoui-MacBook-Pro.local>
2018-09-02 16:40:07 +0900

    feat: Playback NextClip

---
+++ Assets/VideoDemo/Scripts/NextButtonControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButtonControl : ShootableUI {
+
+	public override void ShotClick ()
+	{
+		worldSpaceVideo.setNextClip ();
+	}
+}
--- Assets/VideoDemo/Scripts/WorldSpaceVideo.cs
+++ Assets/VideoDemo/Scripts/WorldSpaceVideo.cs
@@ -9,15 +9,17 @@ public class WorldSpaceVideo : MonoBehaviour {
 	public Material playButtonMaterial;
 	public Material pauseButtonMaterial;
 	public Renderer playButtonRenderer;
+	public VideoClip[] videoClips;
 
 	private VideoPlayer videoPlayer;
+	private int videoClipIndex;
 
 	void Awake(){
 		videoPlayer = GetComponent<VideoPlayer> ();
 	}
 	// Use this for initialization
 	void Start () {
-		
+		videoPlayer.clip = videoClips [0];
 	}
 	
 	// Update is called once per frame
@@ -35,5 +37,16 @@ public class WorldSpaceVideo : MonoBehaviour {
 			videoPlayer.Play ();
 			playButtonRenderer.material = pauseButtonMaterial;
 		}
+	}
+
+	public void setNextClip(){
+		videoClipIndex++;
+
+		if (videoClipIndex >= videoClips.Length) {
+			videoClipIndex = videoClipIndex % videoClips.Length;
+		}
+
+		videoPlayer.clip = videoClips [videoClipIndex];
+		videoPlayer.Play ();
 	}
 }
